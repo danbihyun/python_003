2af669550b01fb68b15b8af00a52f0eac78eea03
danbihyun <sessak02@naver.com>
2025-11-03 16:30:43 +0900

    update

--- day3/practice/final_2.py
+++ day3/practice/final_2.py
@@ -16,11 +16,13 @@ for key, value in menu.items():
 # 사용자 선택 (여기서는 하드코딩)
 choice = "2"
 
-# 선택에 따른 동작
-if choice == "1" in menu:
-    print("회원가입을 선택했습니다.")
-elif choice == "2" in menu:
-    print("로그인을 선택했습니다.")
-else:
-    print("프로그램을 종료합니다.")
-    
+# # 선택에 따른 동작
+# if choice == "1" in menu:
+#     print("회원가입을 선택했습니다.")
+# elif choice == "2" in menu:
+#     print("로그인을 선택했습니다.")
+# else:
+#     print("프로그램을 종료합니다.")
+
+# 간결하게 표현하는 법    
+print(f"{menu[choice]} 버튼을 선택하셨습니다.")
